9723e197bbe6cbf170a3e9336a29753a5afbf783
KirKiller <KirKiller@DESKTOP-1JIA97R>
2024-04-25 02:06:29 +0500

    Добавьте файлы проекта.

---
+++ Картинки/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Картинки
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int pictures = 52;
+            int picturesInRow = 3;
+            int inRow;
+            int remainingPictures;
+            inRow = 52 / 3;
+            remainingPictures = 52 % 3;
+            Console.WriteLine(inRow);
+            Console.WriteLine(remainingPictures);
+
+
+        }
+    }
+}
